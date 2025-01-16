c4a450208b15065764f43f919013831ff7ba87fe
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-16 04:54:30 +0000

    creacion de estructura interna basica de file profile.jsx

---
+++ src/front/js/pages/profile.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState, useEffect} from "react";
+import { Context } from "../store/appContext";
+import "../../styles/login.css";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <></>
+    );
+};
