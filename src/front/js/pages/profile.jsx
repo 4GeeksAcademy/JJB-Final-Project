26bf43c39bee0fdd54b48e539aa3bd75f2038521
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-16 05:44:48 +0000

    creacion del file profile.css y codificacion del css para el frontend de profile

--- src/front/js/pages/profile.jsx
+++ src/front/js/pages/profile.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect} from "react";
 import { Context } from "../store/appContext";
-import "../../styles/login.css";
+import "../../styles/profile.css";
 import { Link } from "react-router-dom";
 
 export const Profile = () => {
@@ -10,62 +10,28 @@ export const Profile = () => {
         <div className="container-fluid">
           <div className="row">
             {/* Sidebar */}
-            <div className="col-md-3 sidebar" style={{ backgroundColor: "#ffe6e6", height: "100vh", paddingTop: "20px" }}>
+            <div className="col-md-3 sidebar">
               <div className="text-center mb-4">
-                <div
-                  className="profile-image"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    backgroundColor: "#ffe6e6",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "50%",
-                    margin: "0 auto",
-                    fontSize: "50px",
-                    color: "#333",
-                  }}
-                >
-                  A
+                <div className="profile-icon">
+                  <span className="icon-initial">A</span>
                 </div>
-                <h6 className="mt-2">Nickname</h6>
-                <p>Email</p>
+                <p className="nickname">Nickname</p>
+                <p className="email">Email</p>
               </div>
-              <div className="list-group">
-                <a href="#" className="list-group-item">
-                  Favorites
-                </a>
-                <a href="#" className="list-group-item">
-                  Payments
-                </a>
-                <a href="#" className="list-group-item">
-                  Subscription
-                </a>
-                <a href="#" className="list-group-item">
-                  Edit Profile
-                </a>
-                <a href="#" className="list-group-item">
-                  Account
-                </a>
-              </div>
+              <ul className="menu-list">
+                <li>Favorites</li>
+                <li>Payments</li>
+                <li>Subscription</li>
+                <li>Edit Profile</li>
+                <li>Account</li>
+              </ul>
             </div>
+    
             {/* Main Content */}
-            <div className="col-md-9 main-content" style={{ padding: "30px" }}>
-              <div className="row">
-                <div className="col-md-4 text-center">
-                  <div
-                    className="profile-image"
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      backgroundColor: "#ffe6e6",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      borderRadius: "50%",
-                    }}
-                  >
+            <div className="col-md-9 main-content">
+              <div className="row align-items-center">
+                <div className="col-md-4">
+                  <div className="profile-image">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="80"
@@ -83,19 +49,37 @@ export const Profile = () => {
                   </div>
                 </div>
                 <div className="col-md-8">
-                  <div className="profile-info">
-                    <h5>Full Name</h5>
-                    <p>Name and lastname</p>
-                    <h5>Birthdate</h5>
-                    <p>01/01/2000</p>
-                    <h5>Email</h5>
-                    <p>test@gmail.com</p>
-                    <h5>Role</h5>
-                    <p>Usuario</p>
-                    <h5>Nickname</h5>
-                    <p>testing</p>
-                    <h5>Membership</h5>
-                    <p>Free</p>
+                  <div className="profile-info text-start">
+                    <div className="row mb-3">
+                      <div className="col-6">
+                        <h6>Full Name</h6>
+                        <p>Name and lastname</p>
+                      </div>
+                      <div className="col-6">
+                        <h6>Birthdate</h6>
+                        <p>01/01/2000</p>
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-6">
+                        <h6>Email</h6>
+                        <p>test@gmail.com</p>
+                      </div>
+                      <div className="col-6">
+                        <h6>Role</h6>
+                        <p>Usuario</p>
+                      </div>
+                    </div>
+                    <div className="row mb-3">
+                      <div className="col-6">
+                        <h6>Nickname</h6>
+                        <p>testing</p>
+                      </div>
+                      <div className="col-6">
+                        <h6>Membership</h6>
+                        <p>Free</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
