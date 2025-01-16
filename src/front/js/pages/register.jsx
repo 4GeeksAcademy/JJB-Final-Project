cb8b04935f182a8b87cca506de16ddefc48ad425
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-16 02:29:59 +0000

    finalizacion de codificacion de frontend de registro

--- src/front/js/pages/register.jsx
+++ src/front/js/pages/register.jsx
@@ -11,13 +11,8 @@ export const Register = () => {
                 <div class="row justify-content-center">
                     <div class="col-lg-4 d-none d-sm-block" id="register">
                         <div class="text-md-center p-5 mt-5">
-                            <h1 class="display-4 fw-bold lh-1 nav-primary-item my-5">Crear cuenta</h1>
-                            <div class="d-md-flex justify-content-md-center mb-4 mb-lg-3">
-                                <button type="button"
-                                    class="color-gradient btn btn-info btn-md fs-2 px-4 me-md-2 fw-bold text-white">
-                                    Registrarse
-                                </button>
-                            </div>
+                            <h1 class="display-4 fw-bold lh-1 nav-primary-item my-5">Conviertete en miembro</h1>
+                            <p class="lead text-black fs-3 mb-5">y disfruta de todos los beneficios</p>
                         </div>
                     </div>
                     <div class="col-lg-4 col-12 bg-white">
@@ -34,21 +29,20 @@ export const Register = () => {
                                         <input type="text" class="form-control fs-5" id="user" name="user"
                                             placeholder="Ingresar email"/>
                                     </div>
-                                    <div class="form mb-5 fs-5">
+                                    <div class="form mb-3 fs-5">
                                         <label for="pass">Contraseña</label>
                                         <input type="password" class="form-control fs-5" id="pass" name="pass"
                                             placeholder="Ingresar contraseña"/>
                                     </div>
-                                    <button class="w-100 btn btn-lg btn-dark" type="submit">Ingresar</button>
+                                    <div class="form mb-5 fs-5">
+                                        <label for="nickname">Nickname</label>
+                                        <input type="text" class="form-control fs-5" id="nickname" name="nickname"
+                                            placeholder="Ingresar un nickname"/>
+                                    </div>
+                                    
+                                    <button class="w-100 btn btn-lg btn-dark" type="submit">Registrarse</button>
                                 </form>
-                                <div class="text-center">
-                                    <button class="w-50 btn btn-lg btn-dark mt-3 d-lg-none color-gradient" type="submit">
-                                        Registrarse
-                                    </button>
-                                    <a href="" class="text-decoration-none">
-                                        <p class="my-4 text-black fs-5">¿Olvidades la contraseña?</p>
-                                    </a>
-                                </div>
+
                             </div>
                         </div>
                     </div>
