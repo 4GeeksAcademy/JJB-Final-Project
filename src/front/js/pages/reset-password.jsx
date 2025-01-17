5faffb5bf9a83b784f46a1c35bd0ae139cb51421
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-01-17 14:56:58 +0000

    se agrega boton de regreso en reset password

--- src/front/js/pages/reset-password.jsx
+++ src/front/js/pages/reset-password.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const ResetPassword = () => {
     const { store, actions } = useContext(Context);
@@ -8,7 +9,7 @@ export const ResetPassword = () => {
     return (
         <div className="text-center mt-5">
             <h1>Aqui se resetea la password</h1>
-
+            <Link to="/">Regresar</Link>
         </div>
     );
 };
