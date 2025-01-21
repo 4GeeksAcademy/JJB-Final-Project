77bbd1e525d3d845bb97ff89811d195b1a9e1105
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-21 02:14:45 +0000

    creacion de forums.css

--- src/front/js/pages/forums.jsx
+++ src/front/js/pages/forums.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect} from "react";
 import { Context } from "../store/appContext";
-import "../../styles/login.css";
+import "../../styles/forums.css";
 import { Link, useNavigate  } from "react-router-dom";
 
 export const forums = () => {
