766c32c00188f3728a3c46507757a8b8f7a19c5c
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-01-21 02:23:03 +0000

    se agrega logo y enlace a home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import logoNav from "../../img/logo_shespace_navbar.png";
+import { Link, useNavigate  } from "react-router-dom";
+
 
 export const Navbar = () => {
 	return (
-		
 		<nav className="navbar bg-body-tertiary">
 			<div className="container-fluid">
-				<a className="navbar-brand" href="#">
-				<img  alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-				Bootstrap
-				</a>
+				<Link className="navbar-brand" to={"/"}>
+					<img 
+						src={logoNav}
+						alt="Logo" 
+						width="30" 
+						height="30" 
+						className="d-inline-block align-text-top" 
+					/>
+					Bootstrap
+				</Link>
 			</div>
 		</nav>
 	);
