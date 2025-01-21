aad9346cc4b9b1ab5f3bdfa22ea9cc1ef2f64d7f
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-01-21 02:32:31 +0000

    se agregan mas secciones

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,7 +5,7 @@ import { Link, useNavigate  } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar bg-body-tertiary">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container-fluid">
 				<Link className="navbar-brand" to={"/"}>
 					<img 
@@ -15,8 +15,21 @@ export const Navbar = () => {
 						height="30" 
 						className="d-inline-block align-text-top" 
 					/>
-					Bootstrap
+					SheSpace
 				</Link>
+				<div className="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Features</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Pricing</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
