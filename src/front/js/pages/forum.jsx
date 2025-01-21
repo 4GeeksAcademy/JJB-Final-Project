3b7a66c809ee45253973aadbbbe9ed85b34f7ff8
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-01-21 15:26:50 +0000

    se agrega boton para crear foro

--- src/front/js/pages/forum.jsx
+++ src/front/js/pages/forum.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext.js";
 import { Link, useNavigate } from "react-router-dom";
 import { ForumCard } from "../component/forumCard.jsx";
+import "../../styles/colors.css";
 
 export const Forums = () => {
     const { store, actions } = useContext(Context);
@@ -9,11 +10,27 @@ export const Forums = () => {
     useEffect(() => {
         actions.loadForums();
     }, []);
+
+    const showModal = () => {
+        
+    }
+    
     
     return (
         <>
             {store.forums.length > 0 ? <ForumCard/> : <h1> No se encontraron Forums </h1>}
-
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center" >
+                        <div className="btn" 
+                            style={{background: 'var( --primary-color)', color: 'var(--text-color)'}}
+                            onClick={showModal}
+                            >
+                            Crear Foro
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
 
     );
