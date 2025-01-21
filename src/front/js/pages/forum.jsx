12c02a236a5980a802c3f7cd404a796d4c21f13b
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-01-21 15:48:51 +0000

    se agrega el modal

--- src/front/js/pages/forum.jsx
+++ src/front/js/pages/forum.jsx
@@ -6,13 +6,14 @@ import "../../styles/colors.css";
 
 export const Forums = () => {
     const { store, actions } = useContext(Context);
+    const [modalShows, setModalShows] = useState(false);
 
     useEffect(() => {
         actions.loadForums();
     }, []);
 
-    const showModal = () => {
-        
+    const toggleModal = () => {
+        setModalShows(!modalShows)
     }
     
     
@@ -24,10 +25,29 @@ export const Forums = () => {
                     <div className="col d-flex justify-content-center" >
                         <div className="btn" 
                             style={{background: 'var( --primary-color)', color: 'var(--text-color)'}}
-                            onClick={showModal}
-                            >
+                            // onClick={toggleModal}
+                            data-bs-toggle="modal" data-bs-target="#myModal">
                             Crear Foro
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade mt-5" id="myModal" tabIndex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <form className="p-3">
+                            <div className="mb-3">
+                                <label htmlFor="nameForum" className="form-label">Nombre del Foro</label>
+                                <input type="text" className="form-control" id="nameForum" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="contentForum" className="form-label">Descripción</label>
+                                <textarea type="text" className="form-control" id="contentForum"/>
+                            </div> 
+                            <div className="d-grid">
+                                <button type="button" className="btn btn-primary btn-block">Crear Foro</button>
+                            </div>                           
+                        </form>
                     </div>
                 </div>
             </div>
