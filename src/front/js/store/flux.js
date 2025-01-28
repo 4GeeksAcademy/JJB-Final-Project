8146c38952bdc69364a0a6ee35b88fb1d2d6c3bb
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-28 18:23:23 +0000

    creacion del objeto advertisingDetail en el store del flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -6,7 +6,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			profile: {}, 
 			forums : [],
 			advertising : [],
-			forumDetails: {},
+			advertisingDetail: {},
 			forumDetails: {},
 		},
 
