0ddd9122f0b2a6aea021615eb4247c5621c0580d
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-04 15:01:21 +0000

    crecion de file invoices.jsx

---
+++ src/front/js/pages/invoices.jsx
@@ -0,0 +1,16 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/profile.css";
+import { Link, useNavigate } from "react-router-dom";
+import { SideBar } from "../component/sideBar.jsx";
+
+export const Invoices = (props) => {
+    const { store, actions } = useContext(Context);
+
+
+    return (
+        <>
+        </>
+      
+  );  
+};
