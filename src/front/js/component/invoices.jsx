86c3419f4790c915d248bc751d5321e1734bb134
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-04 00:49:13 +0000

    creacion de componente invoices.jsx

---
+++ src/front/js/component/invoices.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import Swal from "sweetalert2";
+import "../../styles/forum.css";
+
+export const AdvertisingCard = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <>
+        </>
+
+    );
+};
