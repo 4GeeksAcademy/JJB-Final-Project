6a7499833f17542b0df10259d284cb40a465883e
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-05 03:59:34 +0000

    creacion y codificaion de pagina payments.jsx

---
+++ src/front/js/pages/payments.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/profile.css";
+import { Link, useNavigate } from "react-router-dom";
+import { SideBar } from "../component/sideBar.jsx";
+import { PayPalButton } from "../component/payPalButton.jsx";
+
+export const Payments = (props) => {
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        {/* Sidebar */}
+        <div className="col-md-3 p-0">
+          <SideBar />
+        </div>
+        {/* Main Content */}
+        <div className="col-md-9 main-content">
+          <PayPalButton/>
+        </div>
+      </div>
+    </div>
+  );
+};
