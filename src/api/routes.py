effd7e3bbc8af168790dc31caf273d0b118f6f2d
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-05 05:21:42 +0000

    rollback

--- src/api/routes.py
+++ src/api/routes.py
@@ -19,8 +19,6 @@ cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-
-
 )
 
 @api.route('/hello', methods=['POST', 'GET'])
