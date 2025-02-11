1141e1c01a53f4048373bbbe1cdf97f368741be0
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-02-11 19:37:48 +0000

    le damos mas diseno a login

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/login.css";
-import { Link, useNavigate  } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2'
+import logo from "../../img/logo_shespace_navbar.png";
 
 export const Login = () => {
     const { store, actions } = useContext(Context);
@@ -10,7 +11,7 @@ export const Login = () => {
     const [password, setPassword] = useState('');
     const [emailChanged, setEmailChanged] = useState(false);
     const [passwordChanged, setPasswordChanged] = useState(false);
-    const [failedAttempts, setFailedAttempts] = useState(0); 
+    const [failedAttempts, setFailedAttempts] = useState(0);
     const navigate = useNavigate();
 
     const EmailChanged = (e) => {
@@ -40,17 +41,17 @@ export const Login = () => {
                 title: "Inicio de sesión exitoso",
                 showConfirmButton: false,
                 timer: 2000
-              });
+            });
             navigate("/profile");
 
         } else {
             console.error("FRONT Error al iniciar sesión:", response.error);
             if (response.status == 404) {
                 console.log("response.status:", response.status);
-                setFailedAttempts((prev) => prev + 1);    
-                if (failedAttempts + 1>= 3) {
+                setFailedAttempts((prev) => prev + 1);
+                if (failedAttempts + 1 >= 3) {
                     triggerResetAlert();
-                }else {
+                } else {
                     Swal.fire({
                         position: "top",
                         icon: "error",
@@ -78,7 +79,7 @@ export const Login = () => {
             title: "Redirigiendo para reestablecer contraseña",
             showConfirmButton: false,
             timer: 1500
-          });
+        });
         navigate("/forgot-password");
     };
 
@@ -101,62 +102,61 @@ export const Login = () => {
 
     return (
         <>
-        <div className="container">
-            <div className="row m-5 border p-md-5">
-                <div className="col-12 col-md-6 d-flex justify-content-md-end justify-content-center mb-3">
-                    <div className="card p-md-3" style={{ width: "18rem" }}>
-                        <div className="card-body">
-                            <h1 className="text-end">Aún no</h1>
-                            <p className="mb-4 text-end fs-3">eres parte del cambio?</p>
-                            <div className="d-grid">
-                                <Link to={"/register"} className="btn btn-primary d-grid" style={{ margin: "0" }}>
+            <div className="fluid-container">
+                <div className="row justify-content-center gap-3 mt-5">
+                    <div className="col-md-4 border rounded shadow-sm px-3 px-lg-4 ">
+                        <div className="d-flex flex-column h-100">
+                            <div className="text-center text-md-end p-5 mt-5">
+                                <h1 className="display-4 fw-bold mb-3">Aún no</h1>
+                                <p className="mb-4 fs-3 ">eres parte del cambio?</p>
+                            </div>
+                            <div className="d-grid mt-auto mb-4">
+                                <Link to={"/register"} className="btn-link fs-5 " >
                                     Registrarse
                                 </Link>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-12 col-md-6 d-flex justify-content-md-start justify-content-center">
-                    <div className="card p-md-3 border rounded" style={{ width: "18rem" }}>
-                        <div className="card-body">
-                            <form>
-                                <div className="mb-1">
-                                    <label htmlFor="email" className="form-label">Correo:</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="email"
-                                        placeholder="nombre@ejemplo.com"
-                                        onChange={EmailChanged}
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-1">
-                                    <label htmlFor="password" className="form-label">Contraseña:</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        id="password"
-                                        onChange={PasswordChanged}
-                                        required
-                                    />
-                                </div>
-                                <div className="d-grid">
-                                    <button
-                                        className="btn btn-primary"
-                                        type="button"
-                                        disabled={!passEmailChanged}
-                                        onClick={sendForm}
-                                    >
-                                        Iniciar Sesión
-                                    </button>
-                                </div>
-                            </form>
+                    <div className="col-md-4 border rounded shadow-sm py-4 px-3 px-lg-4 ">
+                        <div className="d-flex justify-content-center">
+                            <img className="mb-4" src={logo} alt="" width="80" height="80" />
                         </div>
+                        <form>
+                            <div className="mb-3 fs-5">
+                                <label htmlFor="email" className="form-label">Correo:</label>
+                                <input
+                                    type="email"
+                                    className="form-control fs-5"
+                                    id="email"
+                                    placeholder="nombre@ejemplo.com"
+                                    onChange={EmailChanged}
+                                    required
+                                />
+                            </div>
+                            <div className="mb-3 fs-5">
+                                <label htmlFor="password" className="form-label">Contraseña:</label>
+                                <input
+                                    type="password"
+                                    className="form-control fs-5"
+                                    id="password"
+                                    onChange={PasswordChanged}
+                                    required
+                                />
+                            </div>
+                            <div className="d-grid mt-auto">
+                                <button
+                                    className="mt-2 fs-5"
+                                    type="button"
+                                    disabled={!passEmailChanged}
+                                    onClick={sendForm}
+                                >
+                                    Iniciar Sesión
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
         </>
 
     );
