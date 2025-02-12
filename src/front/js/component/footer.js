19a707907bb005ba2945578daa34f52de6f5f225
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-12 06:35:54 +0000

    ajuste en el footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer py-0 mb-0 text-center">
+	<footer className="footer py-0 mt-5 mb-0 text-center">
 
 		<p className="pt-3 pb-0 my-0">© 2025 SheSpace. Todos los derechos reservados.</p>
 		<p className="pt-0 pb-3 my-0" id="subtitle">Desarrollado por: Braulio Díaz, Jessica Raya y Jordan Campos</p>
