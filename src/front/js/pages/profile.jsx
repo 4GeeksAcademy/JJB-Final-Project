9b5727598ca18d01e7a40f0a36840856d324bbb8
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-12 06:24:56 +0000

    se guardan la continuacion ajustes en la estetica de profile

--- src/front/js/pages/profile.jsx
+++ src/front/js/pages/profile.jsx
@@ -1,69 +1,52 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/profile.css";
-import { Link, useNavigate } from "react-router-dom";
-import { SideBar } from "../component/sideBar.jsx";
 
-export const Profile = (props) => {
-    const { store, actions } = useContext(Context);
-    const navigate = useNavigate();
-    const [image, setImage] = useState("");
+export const Profile = () => {
+    const { store } = useContext(Context);
 
     return (
-      <div className="container-fluid">
-          <div className="row">    
-              {/* Main Content */}
-              <div className="col-md-9 main-content">
-                  <div className="row align-items-center">
-                      <div className="col-md-4 d-flex justify-content-center">
-                          <div className="profile-image">
-                              <img
-                                  className="profile-avatar"
-                                  src={store.profile.avatar_url}
-                                  alt="Profile"
-                              />
-                          </div>
-                      </div>
-  
-                      <div className="col-md-8">
-                          <div className="profile-info text-start">
-                              <div className="row mb-3">
-                                  <div className="col-6">
-                                      <h6>Nombre completo</h6>
-                                      <p>{`${store.profile.name || "-"} ${store.profile.lastname || "-"}`}</p>
-                                  </div>
-                                  <div className="col-6">
-                                      <h6>Cumpleaños</h6>
-                                      <p>{store.profile.birthdate || "N/A"}</p>
-                                  </div>
-                              </div>
-  
-                              <div className="row mb-3">
-                                  <div className="col-6">
-                                      <h6>Correo</h6>
-                                      <p>{store.profile.email || "N/A"}</p>
-                                  </div>
-                                  <div className="col-6">
-                                      <h6>Rol</h6>
-                                      <p>{store.profile.role || "N/A"}</p>
-                                  </div>
-                              </div>
-  
-                              <div className="row mb-3">
-                                  <div className="col-6">
-                                      <h6>Apodo</h6>
-                                      <p>{store.profile.nickname || "N/A"}</p>
-                                  </div>
-                                  <div className="col-6">
-                                      <h6>Membresía</h6>
-                                      <p>{store.profile.membership || "N/A"}</p>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  );  
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <div className="container bg-white border-container">
+                <div className="row px-0 py-4 text-center">
+                    {/* Imagen */}
+                    <div className="col-md-12 d-flex justify-content-center align-items-center">
+                        <img
+                            className="profile-avatar"
+                            src={store.profile.avatar_url}
+                            alt="Profile"
+                        />
+                    </div>
+
+
+                </div>
+
+                <div className="row px-0 py-4  text-center">
+
+
+                    {/* Información Personal */}
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                        <h3>Nombre completo</h3>
+                        <p>{`${store.profile.name || "-"} ${store.profile.lastname || "-"}`}</p>
+                        <h3>Cumpleaños</h3>
+                        <p>{store.profile.birthdate || "N/A"}</p>
+                        <h3>Correo</h3>
+                        <p>{store.profile.email || "N/A"}</p>
+                    </div>
+
+                    {/* Más Información */}
+                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                        <h3>Rol</h3>
+                        <p>{store.profile.role || "N/A"}</p>
+                        <h3>Apodo</h3>
+                        <p>{store.profile.nickname || "N/A"}</p>
+                        <h3>Membresía</h3>
+                        <p>{store.profile.membership || "N/A"}</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
 };
