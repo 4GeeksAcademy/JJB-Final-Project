dc590c22735e838c30c747bec2451ccaf9be27a4
Jessan23 <jessicaanairayagrez@gmail.com>
2025-02-12 15:18:11 +0000

    detalles de suscription e invoice

--- src/front/js/pages/invoice.jsx
+++ src/front/js/pages/invoice.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext.js";
 import "../../styles/profile.css";
+import "../../styles/invoice.css";
+
 import { Link, useNavigate } from "react-router-dom";
 import { SideBar } from "../component/sideBar.jsx";
 import { PayPalButton } from "../component/payPalButton.jsx";
@@ -18,27 +20,30 @@ export const Invoice = () => {
 };
 
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+  <div className="row w-100 justify-content-center">
+    {/* Main Content */}
+    <div className="col-md-5 main-content text-center">
+      <TermsConditions />
+      <div className="form-check mb-3 d-flex justify-content-center">
+        <input
+          className="form-check-input fs-5 me-2"
+          type="checkbox"
+          id="flexCheckDefault"
+          onChange={CheckboxChanged}
+        />
+       <div className="form-check mb-3 center-label">
+  <input className="form-check-input fs-5 me-2" type="checkbox" value="" id="flexCheckDefault" onChange={CheckboxChanged} />
+  <label className="form-check-label fs-5" htmlFor="flexCheckDefault">
+    Acepto los términos y condiciones anteriores
+  </label>
+</div>
 
-        <div className="col-2">
-
-        </div>
-        {/* Main Content */}
-        <div className="col-md-5 main-content">
-          <TermsConditions />
-          <div className="form-check mb-3 d-flex justify-content-center">
-            <input className="form-check-input fs-5 me-2" type="checkbox" value="" id="flexCheckDefault" onChange={CheckboxChanged}/>
-            <label className="form-check-label fs-5" htmlFor="flexCheckDefault">
-              Acepto los terminos y las condiciones anteriores
-            </label>
-          </div>
-          <PayPalButton disabled={!checkboxChanged}/>
-        </div>
-        <div className="col-2">
-
-        </div>
       </div>
+      <PayPalButton disabled={!checkboxChanged} />
     </div>
+  </div>
+</div>
+
   );
 };
