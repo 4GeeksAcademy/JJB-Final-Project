b72bf75cee9b8faad7a0ef5e555b212e064e6eae
BDiaz-L <79861209+BDiaz-L@users.noreply.github.com>
2025-02-14 03:53:33 +0000

    modificacion de foro

--- src/front/js/component/forumCard.jsx
+++ src/front/js/component/forumCard.jsx
@@ -7,39 +7,38 @@ import FavoriteButton from "../component/FavoriteButton.jsx";
 
 
 export const ForumCard = ({ forum, user }) => {
-    
+
     const { store, actions } = useContext(Context);
     console.log(store.forums);
-    
-    return (
-        
-                    <div className="col-md-4">
-                        <div className="card h-100">
-                            <div className="card-body d-flex flex-column">
-                                {forum.image_url && (
-                                    <img className="mb-3 card-img-top" src={forum.image_url} alt={forum.title} />
-                                )}
-                                <h5 className="card-title">{forum.title}</h5>
-                                <p className="card-text">{forum.content}</p>
-                               
-                                <FavoriteButton
-                id_forum={forum.id_forum} // ID del foro
-                id_advertising={null}     // Si es para publicidad, pasas el ID de la publicidad
-            />
 
-                                
-                                {/* Botón siempre al final */}
-                                <Link to={`/forum/${forum.id_forum}`} 
-                                    className="btn btn-secondary mt-auto ">
-                                        Entrar
-                                </Link>
-                            </div>
-                            <div className="card-footer text-muted">
-                                {forum.creation_date}
-                            </div>
-                        </div>
+    return (
+
+        <div className="col-md-4">
+            <div className="card h-100">
+                <div className="card-body d-flex flex-column">
+                    {forum.image_url && (
+                        <img className="mb-3 card-img-top" src={forum.image_url} alt={forum.title} />
+                    )}
+                    <h5 className="card-title">{forum.title}</h5>
+                    <p className="card-text">{forum.content}</p>
+                    <div className="mt-auto d-flex">
+                        {/* Botón siempre al final */}
+                        <Link to={`/forum/${forum.id_forum}`} 
+                            className="btn mt-auto flex-grow-1 btn-link">
+                            Entrar
+                        </Link>
+                        <FavoriteButton
+                            id_forum={forum.id_forum} // ID del foro
+                            id_advertising={null}     // Si es para publicidad, pasas el ID de la publicidad
+                        />
                     </div>
-                
+                </div>
+                <div className="card-footer text-muted">
+                    {forum.creation_date}
+                </div>
+            </div>
+        </div>
+
     );
 };
 
